5a0492442b724de7b5730955f962279946fa5c80
RaviRanjanKumar08 <rr.ranjankumar9313907615@gmail.com>
2022-01-24 15:03:12 +0530

    Assignment1

---
+++ 21 Jan_Assignment (Arithmetic)/21 Jan_Assignment (Arithmetic)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("21 Jan_Assignment (Arithmetic)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("21 Jan_Assignment (Arithmetic)")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09478254-227a-4c8e-98f0-ac86856a7436")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 21 Jan_Assignment (Array)/21 Jan_Assignment (Array)/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Jan_Assignment__Array_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int i; double d = 0;
+            double sum = 0;
+            double average = 0;
+            Console.Write("Input 10 elements in the array :\n");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nElements in array are: ");
+            for (i = 0; i < 10; i++)
+            {
+                Console.Write("{0}  ", arr[i]);
+
+            }
+            Array.Reverse(arr);
+            Console.Write("\n");
+            Console.WriteLine("array in descending order : ");
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.Write("\n");
+            Array.Sort(arr);
+            Console.WriteLine("array in ascending order : ");
+            foreach (int value in arr) { Console.Write(value + " "); }
+            Console.Write("\n");
+            Console.WriteLine("maximum element of array is = " + arr.Max());
+            Console.WriteLine("minimum element of array is = " + arr.Min());
+
+            Console.Write("\n");
+            for (i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+                average = sum / 10;
+
+
+            }
+            Console.WriteLine("total = " + sum);
+            Console.WriteLine("average = " + average);
+
+
+        }
+    }
+    
+}

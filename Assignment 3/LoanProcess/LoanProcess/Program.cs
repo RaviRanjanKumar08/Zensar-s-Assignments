48181062b52495efeb3eecbf5d76ac69b75ae5dc
RaviRanjanKumar08 <rr.ranjankumar9313907615@gmail.com>
2022-01-28 13:19:32 +0530

    Assignment 3

---
+++ Assignment 3/LoanProcess/LoanProcess/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanProcess
+{
+    class LoanException : ApplicationException
+    {
+        public LoanException(string msg) : base(msg)
+        {
+
+        }
+    }
+    class LoanProcess
+    {
+        public int Loan_No;
+        public string c_name;
+        public int amount;
+        public double emi_amount;
+        public int bal;
+
+        public void calculate_EMI(int amount)
+        {
+            emi_amount = amount * 0.13 * 3;
+        }
+        public void CheckBalance(int bal)
+        {
+            if (bal < emi_amount)
+            {
+                throw (new LoanException("Not Sufficient Balance to Repay Loan"));
+            }
+            else
+            {
+                Console.WriteLine("Congratulation! Loan Process Successful");
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Loan Amount:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Balance:");
+            int b= Convert.ToInt32(Console.ReadLine()); 
+            LoanProcess lp = new LoanProcess();
+            lp.calculate_EMI(a);
+            lp.CheckBalance(b);
+        }
+    }
+}
---
+++ Assignment 3/Ticket_Booking/Ticket_Booking/Ticket_Booking/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Booking
+{
+    class TicketException : ApplicationException
+    {
+        public TicketException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+    class Passenger
+    {
+        public string name;
+        public int age;
+        public void set()
+        {
+            name=Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+        public void show()
+        {
+            Console.WriteLine("Passenger Name:" + name);
+            Console.WriteLine("Passenger Age:" + age);
+        }
+        public void  TicketBooking(int no_of_tickets)
+        {
+            if(no_of_tickets > 5)
+            {
+                throw (new TicketException("You cannot buy more than five tickets"));
+            }
+            else
+            {
+                Console.WriteLine("Ticket Booked Successfully");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while(n<=5)
+            {
+                Passenger p = new Passenger();
+                Console.WriteLine("Enter Passenger Name and Age");
+                p.set();
+                p.show();
+                Console.WriteLine("No. of tickets that booked:");
+                int T = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    p.TicketBooking(T);
+                }
+                catch (TicketException te)
+                {
+                    Console.WriteLine(te.Message);
+                }
+                n++;
+            }
+        }
+    }
+}

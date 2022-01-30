eb575629803992aaf8ffc1f7c1fed0b55f34e625
RaviRanjanKumar08 <rr.ranjankumar9313907615@gmail.com>
2022-01-30 21:40:32 +0530

    Assignment 3

---
+++ Assignment 3/struct_book/struct_book/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_book
+{
+    struct book
+    {
+        private int book_id;
+        public void GetBookDetails(int id)
+        {
+            book_id = id;
+            Console.WriteLine("Book Id: {0}", book_id);
+        }
+    };
+    internal class program
+    {
+         static void Main(string[] args)
+         {
+            int id;
+            Console.WriteLine("Enter the book id: ");
+            id = Convert.ToInt32(Console.ReadLine());
+            book b = new book();
+            b.GetBookDetails(id);
+         }
+    }
+}

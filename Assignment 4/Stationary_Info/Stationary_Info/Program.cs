c30fdfaefd08d628a08db88308dbe739dd037a56
RaviRanjanKumar08 <rr.ranjankumar9313907615@gmail.com>
2022-02-03 16:49:55 +0530

    Assignment 4

---
+++ Assignment 4/Stationary_Info/Stationary_Info/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationary_Info
+{
+    class Product
+    {
+        static void Main()
+        {
+            SortedList<double, string> prod = new SortedList<double, string>();
+            Console.WriteLine("Enter Details of 10 Products:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter product {0} Price and Product {1} Name", i + 1, i + 1);
+                double pr = Convert.ToDouble(Console.ReadLine());
+                string str = Console.ReadLine();
+                prod.Add(pr, str);
+            }
+            Console.WriteLine("Product Details after sort:");
+            foreach (KeyValuePair<double, string> KV in prod)
+            {
+                Console.WriteLine("Product Name: {1} \t ProductName: {0}", KV.Key, KV.Value);
+            }
+        }
+
+    }
+
+}
